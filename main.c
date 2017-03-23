fe8a1199c22d5d2e549775341e0c6fa645af4937
Sandesh <073BCT539.sandesh@pcampus.edu.np>
2017-03-23 14:38:07 +0545

    Added CLI files, from past!

--- main.c
+++ main.c
@@ -6,18 +6,26 @@
 int main(int argc, char *argv[]) {
     gtk_init(&argc, &argv);
 
+    GtkWidget *beginWin;
+
     GtkBuilder *builder;
     GtkWidget  *mainWin;
 
     builder = gtk_builder_new();
-    gtk_builder_add_from_file(builder, "mainScreen.glade",NULL);
-
-    init(builder);
-    mainWin = GTK_WIDGET(gtk_builder_get_object(builder, "mainWin"));
-
-    g_signal_connect(G_OBJECT(mainWin), "destroy", G_CALLBACK(gtk_main_quit), NULL);
-
-    gtk_widget_show(mainWin);
+    gtk_builder_add_from_file(builder, "begin.glade", NULL);
+//
+//
+//    builder = gtk_builder_new();
+//    gtk_builder_add_from_file(builder, "mainScreen.glade",NULL);
+//
+//    init(builder);
+//    mainWin = GTK_WIDGET(gtk_builder_get_object(builder, "mainWin"));
+//
+//    g_signal_connect(G_OBJECT(mainWin), "destroy", G_CALLBACK(gtk_main_quit), NULL);
+//
+//    gtk_widget_show(mainWin);
+    beginWin = GTK_WIDGET(gtk_builder_get_object(builder, "beginWin"));
+    gtk_widget_show(beginWin);
     gtk_main();
 
     return 0;
